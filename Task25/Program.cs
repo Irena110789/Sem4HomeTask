4a58144b7660896ae3abeddad6f32594d2a30eae
Irina <irena-sunny@list.ru>
2023-06-26 20:25:25 +0300

    :initial commit

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Input number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int deg = A;
+
+for(int i = 1; i < B; i++)
+{
+    deg = deg * A;
+}
+Console.WriteLine($"The number A to the power of B is {deg}");
+
+
---
+++ Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void PrintArray(int[] print)
+    {
+        int current = print.Length;
+        int index = 0;
+        while (index < current)
+        {
+            Console.Write(print[index] + " ");
+            index++;
+        }
+    } 
+
+
+int[] array = new int [8 ];
+Console.WriteLine("Input unit 1 ");
+array[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input unit 2 ");
+array[1] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input unit 3 ");
+array[2] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input unit 4 ");
+array[3] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input unit 5 ");
+array[4] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input unit 6 ");
+array[5] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input unit 7 ");
+array[6] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input unit 8 ");
+array[7] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+PrintArray(array);
